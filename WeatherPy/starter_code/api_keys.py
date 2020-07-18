c24580bd5d577e337a15029defde8394a4a5d897
Marianne B <mboerenko@cfl.rr.com>
2020-07-18 15:59:39 -0400

    Finalize Homework Files

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8e820d20f82a0f15cea5bbc1093b52b8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBrUvO3a0sSoarZoMevB7x_Gune_HHjfBw"
